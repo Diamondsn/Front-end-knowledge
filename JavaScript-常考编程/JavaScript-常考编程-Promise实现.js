83d8c15b62bdd57e9f05781795053546113a2dfd
徐力 <1756625653@qq.com>
2019-09-05 20:04:32 +0800

    feat:Promise实现

--- JavaScript-常考编程/JavaScript-常考编程-Promise实现.js
+++ JavaScript-常考编程/JavaScript-常考编程-Promise实现.js
@@ -64,3 +64,161 @@ MyPromise.resolve(10).then(o=>o*10).then(o=>o+10).then(d=>{
 new MyPromise((resolve,reject)=>reject('Error')).catch(e=>{
     console.log(e);
 });
+
+
+
+
+
+class promise {
+	constructor(excutor) {
+		this.state = 'pending';
+		this.result = undefined;
+		this.reason = undefined;
+		this.resolveStack = [];
+		this.rejectStack = [];
+
+		let resolve = result => {
+			if(this.state === 'pending') {
+				this.result = result;
+				this.resolveStack.forEach(fn => fn());
+				this.state = 'fulfilled';
+			}
+		}
+
+		let reject = result => {
+			if(this.state === 'pending') {
+				this.reason = result;
+				this.rejectStack.forEach(fn => fn());
+				this.state = 'rejected';
+			}
+		}
+
+		try {
+			excutor(resolve, reject)
+		} catch(err) {
+			reject(err)
+		}
+
+	}
+
+	then(onFulfilled,onRejected) {
+
+		let promise2 = new promise((resolve,reject) => {
+			if(this.state === 'fulfilled') {
+				let x = onFulfilled(this.result);
+				resolvePromise(promise2, x, resolve, reject);
+			}
+			if(this.state === 'rejected') {
+				let x = onRejected(this.reason);
+				resolvePromise(promise2, x, resolve, reject);
+			}
+			if(this.state === 'pending') {
+				this.resolveStack.push(() => {
+					let x = onFulfilled(this.result);
+					resolvePromise(promise2, x, resolve, reject);
+				})
+				this.rejectStack.forEach(() => {
+					let x = onRejected(this.reason);
+					resolvePromise(promise2, x, resolve, reject);
+				})
+			}
+		})
+
+		return promise2
+	}
+
+}
+
+
+
+
+
+//有异步的Promise
+class promise {
+	constructor(excutor) {
+		this.state = 'pending';
+		this.result = undefined;
+		this.reason = undefined;
+		this.resolveStack = [];
+		this.rejectStack = [];
+
+		let resolve = result => {
+			if(this.state === 'pending') {
+				this.result = result;
+				this.resolveStack.forEach(fn => fn());
+				this.state = 'fulfilled';
+			}
+		}
+
+		let reject = result => {
+			if(this.state === 'pending') {
+				this.reason = result;
+				this.rejectStack.forEach(fn => fn());
+				this.state = 'rejected';
+			}
+		}
+
+		try {
+			excutor(resolve, reject)
+		} catch(err) {
+			reject(err)
+		}
+
+	}
+
+	then(onFulfilled,onRejected) {
+		// onFulfilled如果不是函数，就忽略onFulfilled，直接返回value
+		onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : value => value;
+		// onRejected如果不是函数，就忽略onRejected，直接扔出错误
+		onRejected = typeof onRejected === 'function' ? onRejected : err => { throw err };
+
+		let promise2 = new promise((resolve,reject) => {
+			if(this.state === 'fulfilled') {
+  				// 保证是异步，若执行有错误则进行报错
+				setTimeout(() => {
+					try {
+						let x = onFulfilled(this.result);
+						resolvePromise(promise2, x, resolve, reject);
+					} catch(err) {
+						reject(err)
+					}
+				}, 0);
+			}
+			if(this.state === 'rejected') {
+				setTimeout(() => {
+					try {
+						let x = onRejected(this.reason);
+						resolvePromise(promise2, x, resolve, reject);
+					} catch(err) {
+						reject(err);
+					}
+				}, 0);
+			}
+			if(this.state === 'pending') {
+				this.resolveStack.push(() => {
+					setTimeout(() => {
+						try {
+							let x = onFulfilled(this.result);
+							resolvePromise(promise2, x, resolve, reject);
+						} catch(err) {
+							reject(err);
+						}
+					}, 0);
+				})
+				this.rejectStack.forEach(() => {
+					setTimeout(() => {
+						try {
+							let x = onRejected(this.reason);
+							resolvePromise(promise2, x, resolve, reject);
+						} catch(err) {
+							reject(err);
+						}
+					}, 0);
+				})
+			}
+		})
+
+		return promise2
+	}
+
+}
