414c1bb5e2bb142ff7d7890e4e937279cec8e188
徐力 <1756625653@qq.com>
2019-09-11 12:19:47 +0800

    feat: 常考编程

--- JavaScript-常考编程/JavaScript-常考编程-单例模式.js
+++ JavaScript-常考编程/JavaScript-常考编程-单例模式.js
@@ -8,4 +8,15 @@ function danli(){
     })();
 }
 
+var GetSingle=(function(){
+    var instance;
+    return function (){
+        if(!instance){
+            this.a = 'Hello World'
+            instance = this;
+        }
+        return instance;
+    }
+})();
+
 console.log(danli());
